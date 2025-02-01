f3ece3795bbeb813275838b43a713641214fed48
Devnath03 <jmdevnath.c@gmail.com>
2025-02-01 21:06:41 +0530

    Solve React Issue

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -24,12 +24,30 @@ export default [
       'react-refresh': reactRefresh,
      
     },
+
+    // New Comment Add to solve React Issue
+    
+      "eslint.enable": true,
+      "eslint.options": {
+        "configFile": ".eslintrc.json"
+      },
+    //////////////////////////////////////////
+    
+    
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+     
+      // New Comment Add to solve React Issue
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      ///////////////////////////////////
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,12 +23,29 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+
+    // New Comment Add to solve React Issue
+    
+    "eslint.enable": true,
+    "eslint.options": {
+      "configFile": ".eslintrc.json"
+    },
+  //////////////////////////////////////////
+
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+
+   // New Comment Add to solve React Issue
+   "react/jsx-uses-react": "error",
+   "react/jsx-uses-vars": "error",
+   "react/jsx-uses-react": "off",
+   "react/react-in-jsx-scope": "off",
+   ///////////////////////////////////
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
