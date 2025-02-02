9f91de4a35ef11e7aa872689852885d306be14dc
Devnath03 <jmdevnath.c@gmail.com>
2025-02-02 23:34:05 +0530

    Change Figures

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,8 +26,8 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>+1-212-456-7890</li>
-                    <li>contact@foreveryou.com</li>
+                    <li>+94-774515426</li>
+                    <li>info@starlitglobal.com</li>
                 </ul>
             </div>
 
